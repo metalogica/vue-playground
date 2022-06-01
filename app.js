8364c996fc2cb48f30e37c585270493b03e4e3ce
richard jarram <rjarram@me.com>
2022-05-31 22:31:43 -0400

    feat: copy features

--- app.js
+++ app.js
@@ -4,16 +4,23 @@ const vm = Vue.createApp({
       rotateX: 0, 
       rotateY: 0,
       rotateZ: 0,
-      perspective: 0,
+      perspective: 400,
     }
   },
   computed: {
     boxStyle() {
-      return [
-        {
-          transform: `rotateX(${this.rotateX}deg) rotateY(${this.rotateY}deg) rotateZ(${this.rotateZ}deg) perspective(${this.perspective}px)`
-        }
-      ]
+      return { transform: `perspective(${this.perspective}px) rotateX(${this.rotateX}deg) rotateY(${this.rotateY}deg) rotateZ(${this.rotateZ}deg)` }
     },
   },
+  methods: {
+    resetOptions() {
+      this.rotateX = 0;
+      this.rotateY = 0;
+      this.rotateZ = 0;
+      this.perspective = 400;
+    },
+    copy() {
+      navigator.clipboard.writeText(`X rotation: ${this.rotateX}deg, Y rotation: ${this.rotateY}deg, Z rotation: ${this.rotateZ}deg, perspective: ${this.perspective}px`);
+    }
+  }
 }).mount('#app');
